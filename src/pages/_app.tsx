f624c4f9801860a77edba204d1790429d55d7c8c
Iryna Kolesnyk <i.kolesnyk.goit@gmail.com>
2023-07-27 00:50:43 +0300

    adds theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import type { AppProps } from "next/app";
 import { Global, ThemeProvider } from "@emotion/react";
 
 import { global } from "@/styles/global-styles";
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Global styles={global} />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <Global styles={global} />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </>
   );
 }
