beb539337520e9518b4750a9bac7ac8b195cdee1
Olena Bekhtir <elena.behtir@gmail.com>
2023-10-31 17:53:50 +0200

    add title and icons for main page

--- src/components/HomePageMainBlock/HomePageMainBlock.styled.tsx
+++ src/components/HomePageMainBlock/HomePageMainBlock.styled.tsx
@@ -3,3 +3,10 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   margin-bottom: 32.9rem;
 `;
+
+export const Title = styled.div`
+  font-style: Montserrat;
+  font-size: 2.25rem;
+  text-transform: uppercase;
+  text-align: center;
+`;
